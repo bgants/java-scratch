3999f0a94032d6627839cacb20223b480297195b
Robert.Gants <Robert.Gants@solers.com>
2017-01-20 11:57:07 -0500

    Adding simple REST service.

---
+++ restService/src/main/java/com/test/restServices/TestFormParams.java
@@ -0,0 +1,41 @@
+package com.test.restServices;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("TestFormParams")
+public class TestFormParams {
+	@POST
+	@Path("publish")
+	@Produces(MediaType.TEXT_XML)
+	public Object publish(@FormParam("message")String message,@QueryParam("topicName") String topicName)throws Exception{
+		System.out.println("Message--> " + message);
+		System.out.println("topicName--> " + topicName);
+		return message;
+	}
+	
+	@POST
+	@Path("installRecordReceiveService")
+	@Produces(MediaType.TEXT_XML)
+	public Object installRecordReceiveService(String message) throws Exception{
+		System.out.println("Message--> " + message);
+		
+		return "ok";
+	}
+	
+	@POST
+	@Path("simpleJsonService")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Object simpleJsonService(String message) throws Exception{
+		System.out.println("Message--> " + message);
+		
+		return "ok";
+	}
+	
+}
