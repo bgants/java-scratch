f04f73befac427c24f2886158d3764f7c2dae48a
Robert.Gants <Robert.Gants@solers.com>
2017-11-15 14:56:24 -0500

    added paging.

--- SpringMongoExample/src/main/java/com/gants/controller/RestaurantController.java
+++ SpringMongoExample/src/main/java/com/gants/controller/RestaurantController.java
@@ -3,6 +3,9 @@ package com.gants.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -21,27 +24,29 @@ public class RestaurantController {
 	@Autowired 
 	private RestaurantRepository repo;
 	
-
 	@Autowired 
 	private MongoTemplate mongTemplate;
 	
 	@RequestMapping(value="/restaurants", method = RequestMethod.GET)
-	public List<Restaurant> restaurants() {
-		List<Restaurant> r = repo.findAll();
+	public Page<Restaurant> restaurants(Pageable pageable) {
+		Page<Restaurant> r = repo.findAll(pageable);
 	
 		return r;
 	}
 	
 	@RequestMapping(value="/restaurants/restaurantName", method = RequestMethod.GET)
-	public List<Restaurant> grade(@RequestParam("name") String name) {
+	public Page<Restaurant> grade(@RequestParam("name") String name, Pageable pageable) {
 		if(name.equals("Wendy's")) {
 			name = "Wendy'S";
 		}
 		Query query = new Query();
+		query.with(pageable);
 		query.addCriteria(Criteria.where("name").is(name));
+		Long total = mongTemplate.count(query, Restaurant.class);
 		List<Restaurant> r = mongTemplate.find(query, Restaurant.class);
+		Page<Restaurant> _r = new PageImpl<Restaurant>(r,pageable,total);
 		
-		return r;
+		return _r;
 		
 	}
 	
